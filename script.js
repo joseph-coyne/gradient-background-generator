00391fb8ff86d987f8be1e8e25186a681954619b
josephpcoyne <josephpcoyne@gmail.com>
2019-04-24 14:04:52 -0400

    drys up js

--- script.js
+++ script.js
@@ -4,31 +4,11 @@ var color2 = document.getElementsByName("color2")[0];
 var h3 = document.querySelector("h3");
 var body = document.querySelector("body");
 
-colorOutput1 = "yellow";
-colorOutput2 = "red";
-
-function setColorValue() {
-  h3.innerHTML =
-    "background: linear-gradient(to right, " +
-    color1.value +
-    ", " +
-    color2.value2 +
-    ")";
-}
-
 function changeBackground() {
   body.style.background =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  h3.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", function() {
-  colorOutput1 = color1.value;
-  setColorValue();
-  changeBackground();
-});
-
-color2.addEventListener("input", function() {
-  colorOutput2 = color2.value;
-  setColorValue();
-  changeBackground();
-});
+color1.addEventListener("input", changeBackground);
+color2.addEventListener("input", changeBackground);
